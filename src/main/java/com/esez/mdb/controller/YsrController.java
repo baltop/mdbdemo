b70cb955e8754d60a81a20cad7a995744d29ccc3
godsr <egoist3008@daum.net>
2021-04-05 14:35:55 +0900

    총기함 위젯

--- src/main/java/com/esez/mdb/controller/YsrController.java
+++ src/main/java/com/esez/mdb/controller/YsrController.java
@@ -27,74 +27,6 @@ public class YsrController {
     @Autowired
     private GunInfoRepository gunInfoRepository; 
     
-    @GetMapping("/gun/list")
-    @ResponseBody
-    public List<Data> getMasterUser(@RequestParam(value= "siteId", required = false, defaultValue = "savegun1")String siteId){
-    	List<Data> list=new ArrayList<>();
-    	Data data = new Data();
-    	if(siteId.equals("savegun1")) {
-    		data.setName("보관");
-        	data.setValue(45);
-        	list.add(data);
-        	data = new Data();
-        	data.setName("근무");
-        	data.setValue(10);
-        	list.add(data);
-        	data = new Data();
-        	data.setName("파견");
-        	data.setValue(10);
-        	list.add(data);
-        	data = new Data();
-        	data.setName("출타자");
-        	data.setValue(10);
-        	list.add(data);
-        	data = new Data();
-        	data.setName("훈련");
-        	data.setValue(10);
-        	list.add(data);
-    	}else if(siteId.equals("savegun2")){
-    		data.setName("보관");
-        	data.setValue(45);
-        	list.add(data);
-        	data = new Data();
-        	data.setName("근무");
-        	data.setValue(12);
-        	list.add(data);
-        	data = new Data();
-        	data.setName("파견");
-        	data.setValue(12);
-        	list.add(data);
-        	data = new Data();
-        	data.setName("출타자");
-        	data.setValue(13);
-        	list.add(data);
-        	data = new Data();
-        	data.setName("훈련");
-        	data.setValue(15);
-        	list.add(data);
-    	}else if(siteId.equals("savegun3")){
-    		data.setName("보관");
-        	data.setValue(45);
-        	list.add(data);
-        	data = new Data();
-        	data.setName("근무");
-        	data.setValue(18);
-        	list.add(data);
-        	data = new Data();
-        	data.setName("파견");
-        	data.setValue(19);
-        	list.add(data);
-        	data = new Data();
-        	data.setName("출타자");
-        	data.setValue(15);
-        	list.add(data);
-        	data = new Data();
-        	data.setName("훈련");
-        	data.setValue(20);
-        	list.add(data);
-    	}
-        return list;
-    }
     @GetMapping("/company/list")
     @ResponseBody
     public List<SmartGun> getCompany(@RequestParam(value= "siteId", required = false, defaultValue = "savegun1")String siteId){
@@ -113,24 +45,9 @@ public class YsrController {
     @GetMapping("/table/list")
     @ResponseBody
     public List<GunInfo> getGunInfo(@RequestParam(value= "siteId", required = false, defaultValue = "mytable")String siteId){
-    	System.out.print("이것:"+gunInfoRepository.findAll());
-    	return gunInfoRepository.findbylimit(6, Sort.by("id"));
-    }
-    
-    public class Data{
-    	String name;
-    	Integer value;
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public Integer getValue() {
-			return value;
-		}
-		public void setValue(Integer value) {
-			this.value = value;
-		}
+//    	return gunInfoRepository.findAll();
+    	List<GunInfo> bb = new ArrayList<>();
+    	bb = gunInfoRepository.findByLimit();
+    	return bb;
     }
 }
--- src/main/java/com/esez/mdb/repository/postgres/GunInfoRepository.java
+++ src/main/java/com/esez/mdb/repository/postgres/GunInfoRepository.java
@@ -3,14 +3,17 @@ package com.esez.mdb.repository.postgres;
 
 import java.util.List;
 
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.esez.mdb.model.postgres.GunInfo;
 
 
 public interface GunInfoRepository extends JpaRepository<GunInfo, Integer> {
-	@Query("SELECT P FROM GunInfo P WHERE LIMITED 6")
-	List<GunInfo> findbylimit(Integer id, Sort sort);
+	@Query(value="select * from zium.guninfo order by id desc limit 3;", nativeQuery=true)
+	List<GunInfo> findByLimit();
+
+//	List<GunInfo> findLast6byGunInfo(String id, Sort sort);
+//	@Query("")
+//	List<GunInfo>findLimited(...., new PageRequest(0,10));
 }
